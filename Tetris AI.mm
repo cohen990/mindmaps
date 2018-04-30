f7de6f5d5bf0e5d2d6ceb542a4886ca7e2f849b4
Dan Cohen <dan.cohen@codurance.com>
2018-04-30 16:52:40 +0100

    add tetris AI

---
+++ Tetris AI.mm
@@ -0,0 +1,7 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1525103219501" ID="ID_1583983818" MODIFIED="1525103466621" TEXT="Tetris Without A Human:&#xa;An introduction to unsupervised Q-Learning">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+</node>
+</map>
